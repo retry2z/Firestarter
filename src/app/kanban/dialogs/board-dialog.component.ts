ac92b8fcad7098bbef2ef1301656da09f930e3fb
Hristiyan Hristov <32390803+retry2z@users.noreply.github.com>
2020-08-22 03:23:09 +0300

    fix: css adjustments

--- src/app/kanban/dialogs/board-dialog.component.ts
+++ src/app/kanban/dialogs/board-dialog.component.ts
@@ -6,26 +6,27 @@ import { Board } from '../board.interface';
 @Component({
   selector: 'app-board-dialog',
   template: `
-  <div ngClass="dialog-container">
-    <h1 mat-dialog-title>Board</h1>
+    <div ngClass="dialog-container">
+      <h1 mat-dialog-title>Board</h1>
 
-    <div mat-dialog-content>
-        <p>What shall we call this board?</p>
-        <br />
-        <mat-form-field>
-            <input placeholder="Title" matInput [(ngModel)]="data.title" />
-        </mat-form-field>
+      <div mat-dialog-content>
+          <p>What shall we call this board?</p>
+          <br />
+          <mat-form-field>
+              <input placeholder="Title" matInput [(ngModel)]="data.title" />
+          </mat-form-field>
+      </div>
+      <br />
+
+      <button mat-button (click)="onNoClick()">Cancel</button>
+      <button mat-raised-button color="accent" [mat-dialog-close]="data.title" cdkFocusInitial>
+          Create
+        </button>
     </div>
-    <br />
-
-    <button mat-button (click)="onNoClick()">Cancel</button>
-     <button mat-raised-button color="accent" [mat-dialog-close]="data.title" cdkFocusInitial>
-         Create
-      </button>
-  </div>
   `,
   styles: [
-    '.dialog-container { text-align: center; }'
+    '.dialog-container { text-align: center; }',
+    '.dialog-container button {margin: 8px}',
   ]
 })
 export class BoardDialogComponent {
