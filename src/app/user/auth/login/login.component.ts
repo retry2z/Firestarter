27010dc58db8ef62252daf9a6bd6d23a4c714f35
Hristiyan Hristov <32390803+retry2z@users.noreply.github.com>
2020-08-22 22:39:03 +0300

    implement functionality

--- src/app/user/auth/login/login.component.ts
+++ src/app/user/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UserService } from '../../user.service';
 
 @Component({
   selector: 'app-login',
@@ -9,24 +10,36 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   form: FormGroup;
-  loginInvalid: false;
-
+  loginInvalid: boolean = false;
+  loading: boolean = false;
 
   constructor(
     private fb: FormBuilder,
+    public userService: UserService
   ) { }
 
-  onSubmit() {
+  async onSubmit() {
     if (this.form.invalid) {
       return
     }
 
-    console.log(this.form.value);
+    this.loading = true;
+    const { email, password } = this.form.value;
+
+
+    try {
+      await this.userService.login(email, password);
+      this.loading = false;
+    }
+    catch (e) {
+      this.loginInvalid = true;
+      this.loading = false;
+    }
   }
 
   ngOnInit(): void {
     this.form = this.fb.group({
-      username: ['', [Validators.required, Validators.email]],
+      email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8)]]
     });
   }
--- src/app/user/auth/register/register.component.ts
+++ src/app/user/auth/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { passwordMatch } from '../../../shared/validators/password-match';
+import { UserService } from '../../user.service';
 
 @Component({
   selector: 'app-register',
@@ -10,19 +11,31 @@ import { passwordMatch } from '../../../shared/validators/password-match';
 export class RegisterComponent implements OnInit {
 
   form: FormGroup;
-
   registerInvalid: boolean = false;
+  loading: boolean = false;
 
   constructor(
     private fb: FormBuilder,
+    public userService: UserService
   ) { }
 
-  onSubmit() {
+  async onSubmit() {
     if (this.form.invalid) {
       return
     }
 
-    console.log(this.form.value);
+    this.loading = true;
+    const { email, password } = this.form.value;
+
+
+    try {
+      await this.userService.register(email, password);
+      this.loading = false;
+    }
+    catch (e) {
+      this.registerInvalid = true;
+      this.loading = false;
+    }
   }
 
   ngOnInit(): void {
