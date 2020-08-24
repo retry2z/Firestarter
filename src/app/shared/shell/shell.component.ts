688e94f79b442c6eb9a4b8277cc10501aff3d4fe
Hristiyan Hristov <32390803+retry2z@users.noreply.github.com>
2020-08-24 12:59:33 +0300

    fix: refactoring

--- src/app/shared/shell/shell.component.ts
+++ src/app/shared/shell/shell.component.ts
@@ -1,9 +1,8 @@
 import { Component} from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { shareReplay, map } from 'rxjs/operators';
 import { UserService } from 'src/app/user/user.service';
-import { User } from 'firebase';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,6 +10,7 @@ import { Router } from '@angular/router';
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.scss']
 })
+
 export class ShellComponent {
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe([Breakpoints.Handset])
